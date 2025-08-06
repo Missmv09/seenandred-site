b3c889c8eab9b2235579999efb02e3e43e1f5b49
Missmv09 <missmv@gmail.com>
2025-08-06 15:15:27 -0700

    Check Stripe key before requiring

--- netlify/functions/check-subscription.js
+++ netlify/functions/check-subscription.js
@@ -1,6 +1,14 @@
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
-
 exports.handler = async (event) => {
+  if (!process.env.STRIPE_SECRET_KEY) {
+    const message = 'Stripe secret key is not configured';
+    console.error(message);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: message })
+    };
+  }
+
+  const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
   const email =
     (event.queryStringParameters && event.queryStringParameters.email) ||
     (event.body && JSON.parse(event.body).email);
